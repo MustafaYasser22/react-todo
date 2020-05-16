ed3d4af12c1eb08bb99a01673811b980c99d4890
mustafayasser22 <sasayasser22@gmail.com>
2020-05-16 10:24:10 +0200

    Updates

---
+++ precache-manifest.3e0738a7954783eac8e7211cb84f35a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "e131466c605f949010c3",
+    "url": "/react-todo/static/js/main.e131466c.chunk.js"
+  },
+  {
+    "revision": "9d2004a8395a840ce953",
+    "url": "/react-todo/static/js/1.9d2004a8.chunk.js"
+  },
+  {
+    "revision": "e131466c605f949010c3",
+    "url": "/react-todo/static/css/main.a67dc559.chunk.css"
+  },
+  {
+    "revision": "900a65905e4aa9dda23960f4f1578497",
+    "url": "/react-todo/index.html"
+  }
+];
